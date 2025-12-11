12f7d75f9527398932631a784f7da20121294a75
Bavagowri <bavagowri.s1@gmail.com>
2025-12-11 11:30:02 +0530

    site logo, game view
    updated the site logo
    made the game play screen

--- react-app/src/components/GameCard/GameCard.jsx
+++ react-app/src/components/GameCard/GameCard.jsx
@@ -1,10 +1,9 @@
 import "./GameCard.css";
 
-export default function GameCard({ game }) {
+export default function GameCard({ game, onClick }) {
   return (
-    <div className="game-card">
+    <div className="game-card" onClick={onClick}>
       <img src={game.image} alt={game.title} className="game-card-img" />
-
       <div className="game-card-info">
         <h4 className="game-title">{game.title}</h4>
       </div>

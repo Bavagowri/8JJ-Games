69a89b9ec69f4e65972f2ef84ee06b68b89fd347
Bavagowri <bavagowri.s1@gmail.com>
2025-12-12 12:13:46 +0530

    proxy + fetch changes
    to access in local

--- react-app/vite.config.js
+++ react-app/vite.config.js
@@ -6,9 +6,9 @@ export default defineConfig({
   server: {
     proxy: {
       "/api": {
-        target: "http://localhost:3000",
-        changeOrigin: true,
-      },
-    },
-  },
+        target: "http://localhost:5174",
+        changeOrigin: true
+      }
+    }
+  }
 });

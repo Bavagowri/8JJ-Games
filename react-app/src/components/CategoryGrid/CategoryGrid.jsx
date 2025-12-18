4317b456395e42479f707bdb2207c97afa1f73d4
Bavagowri <bavagowri.s1@gmail.com>
2025-12-18 11:06:35 +0530

    background change

--- react-app/src/components/CategoryGrid/CategoryGrid.jsx
+++ react-app/src/components/CategoryGrid/CategoryGrid.jsx
@@ -30,23 +30,6 @@ const categories = [
   { id: "all", label: "all", image: "/images/category-grid/all.png" }
 ];
 
-  
-  // { id: "action", icon: "🥊", label: "action", isRoute: false },
-  // { id: "top-picks", icon: "🌶️", label: "topPicks", isRoute: false },
-  // { id: "platformer", icon:"🏃", label: "platformer", isRoute: false },
-  // { id: "halloween_games", icon: "🎃", label: "halloween", isRoute: false },
-  // { id: "card_games", icon: "🃏", label: "card", isRoute: false },
-  // { id: "football_games", icon: "⚽", label: "football", isRoute: false },
-  // { id: "basketball_games", icon: "🏀", label: "basketball", isRoute: false },
-  // { id: "simulation_games", icon: "🎮", label: "simulation", isRoute: false },
-  // { id: "skill_games", icon: "🎯", label: "skill", isRoute: false },
-  // { id: "horror_games", icon: "💀", label: "horror", isRoute: false },
-  // { id: "endless_runner", icon: "🏃", label: "endlessRunner", isRoute: false },
-  // { id: "puzzles", icon: "🧩", label: "puzzles", isRoute: false },
-  // { id: "/all-games", icon: "👾", label: "allGames", isRoute: true },
-  // { id: "faqSection", icon: "❓", label: "faq", isRoute: false }
-
-
 export default function CategoryGrid() {
     const { lang } = useLanguage();
 
